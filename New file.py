4ba144346ee4e923502654a88f35e7898b069aac
Tommyjay25 <71994893+Tommyjay25@users.noreply.github.com>
2023-02-16 23:41:45 -0400

    Update New file.py

--- New file.py
+++ New file.py
@@ -6,7 +6,7 @@ from kivy.lang import Builder
 from kivymd.uix.textfield import MDTextField
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivymd.uix.button import MDFlatButton, MDRaisedButton
+from kivymd.uix.button import MDFlatButton, MDRaisedButton,MDFillRoundFlatButton
 from kivymd.uix.dialog import MDDialog
 from kivy.uix.label import Label
 from kivymd.uix.menu import MDDropdownMenu
@@ -63,16 +63,28 @@ class Process(MDApp):
             width_mult=4,
         )
 
+
     def menu_callback(self, x):
-        self.add_flavor=Label(size_hint=(0.1,0.1),pos_hint={"x":0.15,"y":0.48},color=(0,0,0,1))
-        self.add_flavor.text=f'{x}'
-        self.root.ids.pagina_principal.add_widget(self.add_flavor)
-        self.number_of_units=MDTextField(size_hint=(0.08,0.08),pos_hint={"x":0.08,"y":0.46},on_text_validate=self.accept)
-        self.root.ids.pagina_principal.add_widget(self.number_of_units)
-        self.menu.dismiss()
+        try:
+            self.root.ids.pagina_principal.remove_widget(self.add_flavor)
+            self.root.ids.pagina_principal.remove_widget(self.number_of_units)
+            self.add_flavor=Label(size_hint=(0.1,0.1),pos_hint={"x":0.15,"y":0.48},color=(0,0,0,1))
+            self.add_flavor.text=f'{x}'
+            self.root.ids.pagina_principal.add_widget(self.add_flavor)
+            self.number_of_units=MDTextField(size_hint=(0.08,0.08),pos_hint={"x":0.08,"y":0.46},on_text_validate=self.accept)
+            self.root.ids.pagina_principal.add_widget(self.number_of_units)
+            self.menu.dismiss()
+        except:
+            self.add_flavor = Label(size_hint=(0.1, 0.1), pos_hint={"x": 0.15, "y": 0.48}, color=(0, 0, 0, 1))
+            self.add_flavor.text = f'{x}'
+            self.root.ids.pagina_principal.add_widget(self.add_flavor)
+            self.number_of_units = MDTextField(size_hint=(0.08, 0.08), pos_hint={"x": 0.08, "y": 0.46},
+                                               on_text_validate=self.accept)
+            self.root.ids.pagina_principal.add_widget(self.number_of_units)
+            self.menu.dismiss()
     def accept(self,*args):
         if self.number_of_units.text!="":
-            self.btn_accept=Button(size_hint=(0.08,0.05),pos_hint={"x":0.18,"y":0.46},text="accept",on_release=self.add_to_RMWH)
+            self.btn_accept=MDRaisedButton(size_hint=(0.08,0.05),pos_hint={"x":0.18,"y":0.46},text="accept",on_release=self.add_to_RMWH)
             self.root.ids.pagina_principal.add_widget(self.btn_accept)
         else:
             pass
@@ -104,35 +116,35 @@ class Process(MDApp):
             if self.RM_containers[self.flavor] >= 180:
                 try:
                     self.root.ids.pagina_principal.remove_widget(self.raw_mat_quantities)
-                    self.raw_mat_quantities = Label(pos_hint={"x": 0.15, "y": 0.75}, size_hint=(0.10, 0.05), color=(0, 0, 0, 1))
+                    self.raw_mat_quantities = Label(pos_hint={"x": 0.15, "y": 0.72}, size_hint=(0.10, 0.05), color=(0, 0, 0, 1))
                     self.raw_mat_quantities.text = f'Warehouse stock:\n {round(self.RM_containers[self.flavor], 2)} containers\nof {self.flavor} juice' \
                                                    f'\nStock: \n Lemon: {round(float(self.RM_containers["Lemon"]),2)}\n Pineapple: {round(float(self.RM_containers["Pineapple"]),2)}\n Orange: {round(float(self.RM_containers["Orange"]),2)}'
                     self.root.ids.pagina_principal.add_widget(self.raw_mat_quantities)
                 except:
-                    self.raw_mat_quantities = Label(pos_hint={"x": 0.15, "y": 0.75}, size_hint=(0.10, 0.05), color=(0, 0, 0, 1))
+                    self.raw_mat_quantities = Label(pos_hint={"x": 0.15, "y": 0.72}, size_hint=(0.10, 0.05), color=(0, 0, 0, 1))
                     self.raw_mat_quantities.text = f'Warehouse stock:\n {round(self.RM_containers[self.flavor], 2)} containers\nof {self.flavor} juice' \
                                                    f'\nStock: \n Lemon: {round(float(self.RM_containers["Lemon"]), 2)}\n Pineapple: {round(float(self.RM_containers["Pineapple"]), 2)}\n Orange: {round(float(self.RM_containers["Orange"]), 2)}'
                     self.root.ids.pagina_principal.add_widget(self.raw_mat_quantities)
             else:
                 try:
                     self.root.ids.pagina_principal.remove_widget(self.raw_mat_quantities)
-                    self.raw_mat_quantities = Label(pos_hint={"x": 0.15, "y": 0.75}, size_hint=(0.10, 0.05),
+                    self.raw_mat_quantities = Label(pos_hint={"x": 0.15, "y": 0.72}, size_hint=(0.10, 0.05),
                                                     color=(0, 0, 0, 1))
                     self.raw_mat_quantities.text = f'Add stock of {self.flavor} \nStock: \n Lemon: {round(float(self.RM_containers["Lemon"]), 2)}\n Pineapple: {round(float(self.RM_containers["Pineapple"]), 2)}\n Orange: {round(float(self.RM_containers["Orange"]), 2)}'
                     self.root.ids.pagina_principal.add_widget(self.raw_mat_quantities)
                 except:
-                    self.raw_mat_quantities = Label(pos_hint={"x": 0.15, "y": 0.75}, size_hint=(0.10, 0.05),
+                    self.raw_mat_quantities = Label(pos_hint={"x": 0.15, "y": 0.72}, size_hint=(0.10, 0.05),
                                                     color=(0, 0, 0, 1))
                     self.raw_mat_quantities.text = f'Add stock of {self.flavor} \nStock: \n Lemon: {round(float(self.RM_containers["Lemon"]), 2)}\n Pineapple: {round(float(self.RM_containers["Pineapple"]), 2)}\n Orange: {round(float(self.RM_containers["Orange"]), 2)}'
                     self.root.ids.pagina_principal.add_widget(self.raw_mat_quantities)
         except:
             try:
                 self.root.ids.pagina_principal.remove_widget(self.raw_mat_quantities)
-                self.raw_mat_quantities = Label(pos_hint={"x": 0.15, "y": 0.75}, size_hint=(0.10, 0.05), color=(0, 0, 0, 1))
+                self.raw_mat_quantities = Label(pos_hint={"x": 0.15, "y": 0.72}, size_hint=(0.10, 0.05), color=(0, 0, 0, 1))
                 self.raw_mat_quantities.text = f'\nStock: \n Lemon: {round(float(self.RM_containers["Lemon"]), 2)}\n Pineapple: {round(float(self.RM_containers["Pineapple"]), 2)}\n Orange: {round(float(self.RM_containers["Orange"]), 2)}'
                 self.root.ids.pagina_principal.add_widget(self.raw_mat_quantities)
             except:
-                self.raw_mat_quantities = Label(pos_hint={"x": 0.15, "y": 0.75}, size_hint=(0.10, 0.05),
+                self.raw_mat_quantities = Label(pos_hint={"x": 0.15, "y": 0.72}, size_hint=(0.10, 0.05),
                                                 color=(0, 0, 0, 1))
                 self.raw_mat_quantities.text = f'\nStock: \n Lemon: {round(float(self.RM_containers["Lemon"]), 2)}\n Pineapple: {round(float(self.RM_containers["Pineapple"]), 2)}\n Orange: {round(float(self.RM_containers["Orange"]), 2)}'
                 self.root.ids.pagina_principal.add_widget(self.raw_mat_quantities)
@@ -324,7 +336,7 @@ class Process(MDApp):
     def Summary(self):
         try:
             self.root.ids.pagina_principal.remove_widget(self.Totals)
-            self.Totals=Label(pos_hint={"x": 0.65, "y": 0.35}, size_hint=(0.30, 0.40), color=(0, 0, 0, 1))
+            self.Totals=Label(pos_hint={"x": 0.65, "y": 0.40}, size_hint=(0.30, 0.40), color=(0, 0, 0, 1))
             self.Totals.text = f'Total hours: {round((round(sum(self.Total_hours_list_s1), 2) + round(sum(self.Total_hours_list_s2), 2) + round(sum(self.Total_hours_list_s3), 2) + round(sum(self.noice_time_list), 2)), 2)} hours' \
                                f'\n  Stage 1: {round(sum(self.Total_hours_list_s1), 2)}\n  Stage 2: {round(sum(self.Total_hours_list_s2), 2)}' \
                                f'\n  Stage 3: {round(sum(self.Total_hours_list_s3), 2)}\nUnproductive_hours: {round(sum(self.noice_time_list), 2)}' \
@@ -334,7 +346,7 @@ class Process(MDApp):
 
             self.root.ids.pagina_principal.add_widget(self.Totals)
         except:
-            self.Totals = Label(pos_hint={"x": 0.65, "y": 0.35}, size_hint=(0.30, 0.40), color=(0, 0, 0, 1))
+            self.Totals = Label(pos_hint={"x": 0.65, "y": 0.40}, size_hint=(0.30, 0.40), color=(0, 0, 0, 1))
             self.Totals.text = f'Total hours: {round((round(sum(self.Total_hours_list_s1), 2) + round(sum(self.Total_hours_list_s2), 2) + round(sum(self.Total_hours_list_s3), 2) + round(sum(self.noice_time_list), 2)),2)} hours' \
                                f'\n  Stage 1: {round(sum(self.Total_hours_list_s1), 2)}\n  Stage 2: {round(sum(self.Total_hours_list_s2), 2)}' \
                                f'\n  Stage 3: {round(sum(self.Total_hours_list_s3), 2)}\nUnproductive_hours: {round(sum(self.noice_time_list), 2)}' \
@@ -344,18 +356,22 @@ class Process(MDApp):
 
             self.root.ids.pagina_principal.add_widget(self.Totals)
     month_list=[]
+
+
     def real_demand(self):
         try:
-            self.month=1
-            print(self.month)
-            """Product demand"""
-            self.montly_Demand = {"product_1": rnd.randrange(15000, 19000), "product_2": rnd.randrange(20000, 23000),
-                             "product_3": rnd.randrange(22000, 24000)}
-            self.month_list.append(self.month)
+            self.root.ids.pagina_principal.remove_widget(self.delivery)
             self.root.ids.pagina_principal.remove_widget(self.request)
-            self.request=Label(pos_hint={"x":0.75,"y":0.15},size_hint=(0.1,0.1),color=(0,0,0,1))
-            self.total_month = sum(self.month_list) - 1
-            self.request.text = f"Month: {self.total_month}\nLemon: {self.montly_Demand['product_1']*self.total_month}\nPineapple: {self.montly_Demand['product_2']*self.total_month}\nOrange: {self.montly_Demand['product_3']*self.total_month}"
+            """Product demand"""
+            self.month = 1
+            self.montly_Demand = {"product_1": rnd.randrange(15000, 19000), "product_2": rnd.randrange(20000, 22800),
+                                  "product_3": rnd.randrange(22000, 23800)}
+            self.month_list.append(self.month)
+            self.request = Label(pos_hint={"x": 0.75, "y": 0.12}, size_hint=(0.1, 0.1), color=(0, 0, 0, 1))
+            self.total_month = sum(self.month_list)
+            self.request.text = f"Month: {self.total_month}\nLemon: {self.montly_Demand['product_1'] * self.total_month}\nPineapple: {self.montly_Demand['product_2'] * self.total_month}\nOrange: {self.montly_Demand['product_3'] * self.total_month}"
+            self.delivery = MDRaisedButton(pos_hint={"x": 0.90, "y": 0.15}, size_hint=(0.08, 0.05), text="Deliver",on_release=self.deliver)
+            self.root.ids.pagina_principal.add_widget(self.delivery)
             self.root.ids.pagina_principal.add_widget(self.request)
 
         except:
@@ -364,10 +380,93 @@ class Process(MDApp):
             self.montly_Demand = {"product_1": rnd.randrange(15000, 19000), "product_2": rnd.randrange(20000, 23000),
                              "product_3": rnd.randrange(22000, 24000)}
             self.month_list.append(self.month)
-            self.request = Label(pos_hint={"x": 0.75, "y": 0.15}, size_hint=(0.1, 0.1), color=(0, 0, 0, 1))
-            self.total_month=sum(self.month_list)-1
+            self.request = Label(pos_hint={"x": 0.75, "y": 0.12}, size_hint=(0.1, 0.1), color=(0, 0, 0, 1))
+            self.total_month=sum(self.month_list)
             self.request.text = f"Month: {self.total_month}\nLemon: {self.montly_Demand['product_1']*self.total_month}\nPineapple: {self.montly_Demand['product_2']*self.total_month}\nOrange: {self.montly_Demand['product_3']*self.total_month}"
+            self.delivery=MDRaisedButton(pos_hint={"x": 0.90, "y": 0.15}, size_hint=(0.08, 0.05),text="Deliver",on_release=self.deliver)
+            self.root.ids.pagina_principal.add_widget(self.delivery)
             self.root.ids.pagina_principal.add_widget(self.request)
+
+    """This button will help us to take product from warehouse to the customer"""
+
+    def deliver(self, *args):
+        try:
+            try:
+                self.root.ids.pagina_principal.remove_widget(self.Lemon_deficit)
+            except:
+                pass
+            try:
+                self.root.ids.pagina_principal.remove_widget(self.Pineapple_deficit)
+            except:
+                pass
+            try:
+                self.root.ids.pagina_principal.remove_widget(self.Orange_deficit)
+            except:
+                pass
+            """self.Total_units_p2
+            self.Total_units_p3"""
+            if int(sum(self.Total_units_p1)) > (self.montly_Demand['product_1'] * self.total_month):
+                Deliver_p1=-(int(self.montly_Demand['product_1'] * self.total_month))
+                self.Total_units_p1.append(Deliver_p1)
+            elif int(sum(self.Total_units_p1)) < (self.montly_Demand['product_1'] * self.total_month):
+                Deliver_p1 = -int(sum(self.Total_units_p1))
+                self.Total_units_p1.append(Deliver_p1)
+                self.Lemon_deficit = Label(pos_hint={"x": 0.75, "y": 0.08}, size_hint=(0.1, 0.05), color=(0, 0, 0, 1))
+                self.Lemon_deficit.text=(f"Lemon juice units deficit {(int(self.montly_Demand['product_1'] * self.total_month))-int(sum(self.Total_units_p1))}")
+                self.root.ids.pagina_principal.add_widget(self.Lemon_deficit)
+            if int(sum(self.Total_units_p2)) > (self.montly_Demand['product_2'] * self.total_month):
+                Deliver_p2 = -(int(self.montly_Demand['product_1'] * self.total_month))
+                self.Total_units_p2.append(Deliver_p2)
+            elif int(sum(self.Total_units_p2)) < (self.montly_Demand['product_2'] * self.total_month):
+                Deliver_p2 = -int(sum(self.Total_units_p2))
+                self.Total_units_p2.append(Deliver_p2)
+                self.Pineapple_deficit = Label(pos_hint={"x": 0.75, "y": 0.06}, size_hint=(0.1, 0.05), color=(0, 0, 0, 1))
+                self.Pineapple_deficit.text = (
+                    f"Pineapple juice units deficit {(int(self.montly_Demand['product_2'] * self.total_month)) - int(sum(self.Total_units_p2))}")
+                self.root.ids.pagina_principal.add_widget(self.Pineapple_deficit)
+            if int(sum(self.Total_units_p3)) > (self.montly_Demand['product_3'] * self.total_month):
+                Deliver_p3=-(int(self.montly_Demand['product_3'] * self.total_month))
+                self.Total_units_p3.append(Deliver_p3)
+            elif int(sum(self.Total_units_p3)) < (self.montly_Demand['product_3'] * self.total_month):
+                Deliver_p3 = -int(sum(self.Total_units_p3))
+                self.Total_units_p3.append(Deliver_p3)
+                self.Orange_deficit = Label(pos_hint={"x": 0.75, "y": 0.04}, size_hint=(0.1, 0.05), color=(0, 0, 0, 1))
+                self.Orange_deficit.text=(f"Orange juice units deficit {(int(self.montly_Demand['product_3'] * self.total_month))-int(sum(self.Total_units_p3))}")
+                self.root.ids.pagina_principal.add_widget(self.Orange_deficit)
+        except:
+            """self.Total_units_p2
+                        self.Total_units_p3"""
+            if int(sum(self.Total_units_p1)) > (self.montly_Demand['product_1'] * self.total_month):
+                Deliver_p1 = -(int(self.montly_Demand['product_1'] * self.total_month))
+                self.Total_units_p1.append(Deliver_p1)
+            elif int(sum(self.Total_units_p1)) < (self.montly_Demand['product_1'] * self.total_month):
+                Deliver_p1 = -int(sum(self.Total_units_p1))
+                self.Total_units_p1.append(Deliver_p1)
+                self.Lemon_deficit = Label(pos_hint={"x": 0.75, "y": 0.08}, size_hint=(0.1, 0.05), color=(0, 0, 0, 1))
+                self.Lemon_deficit.text = (
+                    f"Lemon juice units deficit {(int(self.montly_Demand['product_1'] * self.total_month)) - int(sum(self.Total_units_p1))}")
+                self.root.ids.pagina_principal.add_widget(self.Lemon_deficit)
+            if int(sum(self.Total_units_p2)) > (self.montly_Demand['product_2'] * self.total_month):
+                Deliver_p2 = -(int(self.montly_Demand['product_1'] * self.total_month))
+                self.Total_units_p2.append(Deliver_p2)
+            elif int(sum(self.Total_units_p2)) < (self.montly_Demand['product_2'] * self.total_month):
+                Deliver_p2 = -int(sum(self.Total_units_p2))
+                self.Total_units_p2.append(Deliver_p2)
+                self.Pineapple_deficit = Label(pos_hint={"x": 0.75, "y": 0.06}, size_hint=(0.1, 0.05),
+                                               color=(0, 0, 0, 1))
+                self.Pineapple_deficit.text = (
+                    f"Pineapple juice units deficit {(int(self.montly_Demand['product_2'] * self.total_month)) - int(sum(self.Total_units_p2))}")
+                self.root.ids.pagina_principal.add_widget(self.Pineapple_deficit)
+            if int(sum(self.Total_units_p3)) > (self.montly_Demand['product_3'] * self.total_month):
+                Deliver_p3 = -(int(self.montly_Demand['product_3'] * self.total_month))
+                self.Total_units_p3.append(Deliver_p3)
+            elif int(sum(self.Total_units_p3)) < (self.montly_Demand['product_3'] * self.total_month):
+                Deliver_p3 = -int(sum(self.Total_units_p3))
+                self.Total_units_p3.append(Deliver_p3)
+                self.Orange_deficit = Label(pos_hint={"x": 0.75, "y": 0.04}, size_hint=(0.1, 0.05), color=(0, 0, 0, 1))
+                self.Orange_deficit.text = (
+                    f"Orange juice units deficit {(int(self.montly_Demand['product_3'] * self.total_month)) - int(sum(self.Total_units_p3))}")
+                self.root.ids.pagina_principal.add_widget(self.Orange_deficit)
 
     """This is where we're going to define projected demand for product 1"""
     def projected_demand_p1(self,args):
@@ -512,10 +611,10 @@ class Process(MDApp):
 
 
     def get_demand(self):
-        self.Product_1=Button(pos_hint={"x":0.15,"y":0.25},size_hint=(0.15,0.05),text="Lemon \n juice", on_release=self.projected_demand_p1)
-        self.Product_2 = Button(pos_hint={"x": 0.15, "y": 0.30}, size_hint=(0.15, 0.05), text="Pineapple \n juice",on_release=self.projected_demand_p2)
-        self.Product_3 = Button(pos_hint={"x": 0.15, "y": 0.35}, size_hint=(0.15, 0.05), text="Orange \n juice",on_release=self.projected_demand_p3)
-        self.demand=Label(pos_hint={"x":0.15,"y":0.2},size_hint=(0.15,0.05),color=(0,0,0,1))
+        self.Product_1=MDFillRoundFlatButton(pos_hint={"x":0.15,"y":0.23},size_hint=(0.15,0.02),text="Lemon \n juice", on_release=self.projected_demand_p1)
+        self.Product_2 = MDFillRoundFlatButton(pos_hint={"x": 0.15, "y": 0.30}, size_hint=(0.15, 0.02), text="Pineapple \n juice",on_release=self.projected_demand_p2)
+        self.Product_3 = MDFillRoundFlatButton(pos_hint={"x": 0.15, "y": 0.37}, size_hint=(0.15, 0.02), text="Orange \n juice",on_release=self.projected_demand_p3)
+        self.demand=Label(pos_hint={"x":0.15,"y":0.15},size_hint=(0.15,0.05),color=(0,0,0,1))
         self.root.ids.pagina_principal.add_widget(self.Product_1)
         self.root.ids.pagina_principal.add_widget(self.Product_2)
         self.root.ids.pagina_principal.add_widget(self.Product_3)
